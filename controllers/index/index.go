52172bbf80529b3344377b3a67eb123e7b3cd1b1
zhangweihao1997 <409541523@qq.com>
2022-01-12 16:37:28 +0800

    finsh QRcode and picture cut demo

--- controllers/index/index.go
+++ controllers/index/index.go
@@ -1,11 +1,35 @@
 package index
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"github.com/hunterhug/go_image"
+	"github.com/skip2/go-qrcode"
+)
 
 type IndexController struct {
 	beego.Controller
 }
 
 func (c *IndexController)Get()  {
+
+	//实现图片截切 按宽度进行比例缩放，输入输出都是
+	filename := "static/upload/a.jpg"
+	savepath := "static/upload/a_800.jpg"
+	err1 := go_image.ScaleF2F(filename,savepath,800)
+	if err1 != nil{
+		beego.Error(err1)
+	}
+
+	//按宽度和高度进行比例缩放，输入和输出都是文件
+	// filename := "static/upload/b.png"
+	// savepath := "static/upload/b_400.png"
+	// err := ThumbnailF2F(filename, savepath, 400, 400)
+	// if err != nil {
+	// 	beego.Error(err)
+	// }
+	err2 := qrcode.WriteFile("https://www.baidu.com",qrcode.Medium,800,"static/upload/qr.png")
+	if err2 != nil{
+		beego.Error("生成二维码失败")
+	}
 	c.TplName = "index/index.html"
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.15
 
 require (
 	github.com/astaxie/beego v1.12.1
+	github.com/hunterhug/go_image v1.0.1
 	github.com/jinzhu/gorm v1.9.16
 	github.com/lib/pq v1.7.0 // indirect
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/smartystreets/goconvey v1.6.4
 	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
